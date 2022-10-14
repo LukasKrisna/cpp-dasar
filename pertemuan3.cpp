bbb1a94d77f93b821c68643167016675601b531e
LukasKrisna <krisnaprasriyan@gmail.com>
2022-10-14 11:17:05 +0700

    pertemuan 3

--- pertemuan3.cpp
+++ pertemuan3.cpp
@@ -4,10 +4,20 @@ using namespace std;
 
 int main()
 {
-    for (int counter = 0; counter <= 8; counter++)
+    cout << "========FOR LOOP========" << endl;
+    for (int counter = 0; counter <= 2; counter++)
     {
         cout << "Hai" << endl;
         cout << "Halo" << endl;
+    }
+
+    cout << "========WHILE LOOP========" << endl;
+
+    int counter = 1;
+    while (counter <= 2)
+    {
+        cout << "Hai" << endl;
+        counter++;
     }
 
     return 0;
