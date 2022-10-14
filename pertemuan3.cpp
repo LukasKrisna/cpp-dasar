be0a31164dcdacd2af130c59724ff44f6bb90190
LukasKrisna <krisnaprasriyan@gmail.com>
2022-10-14 10:38:33 +0700

    pertemuan 3

---
+++ pertemuan3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int counter = 0; counter <= 8; counter++)
+    {
+        cout << "Hai" << endl;
+        cout << "Halo" << endl;
+    }
+
+    return 0;
+}
